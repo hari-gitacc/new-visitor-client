182de90509d4a5cdee77284b8df53fe41a4cd23a
hari-gitacc <harisrini777@gmail.com>
2025-07-25 01:18:41 +0530

    mail

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,10 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-indigo-700 to-purple-900 flex flex-col items-center justify-center p-4">
-      <div className="w-full max-w-md absolute top-4 right-4 sm:top-6 sm:right-6">
+      <div className="w-full max-w-md absolute top-4 right-[-20px] sm:top-6 sm:right-6">
         {!hideAdminLoginButton && (
           <Link to="/admin/login">
-            <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-full shadow-lg transition duration-200">
+            <button className="bg-indigo-600 hover:bg-indigo-700 text-[12px] text-white font-bold py-1 px-2 rounded-full shadow-lg transition duration-200">
               Admin Login
             </button>
           </Link>
